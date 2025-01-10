b69903b51e974758d454bba28a20ed47785a5f18
ggowtam <Gowtamssg@gmail.com>
2025-01-09 19:31:40 -0500

    uptaded

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 import { CartProvider } from './context/CartContext';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'; 
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Book from './pages/Book';
