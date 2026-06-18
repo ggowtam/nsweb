056fe5ba3285d221ae53ff5cfe49fb73f30068bd
ggowtam <Gowtamssg@gmail.com>
2026-06-18 00:45:30 -0500

    deployed webs URL navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { CartProvider } from './context/CartContext';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'; 
+import { Routes, Route, useLocation } from 'react-router-dom'; 
 
 import Home from './pages/Home';
 import About from './pages/About';
